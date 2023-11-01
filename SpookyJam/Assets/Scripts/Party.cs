bddadb1cd025754616e3b770b53b511de0c58afe
rhonig22 <ryan.honig22@outlook.com>
2023-10-31 17:52:27 -0700

    Post Jam Update
    - tweak levels 4 and 5
    - make level 6
    - update end scene

--- SpookyJam/Assets/Scripts/Party.cs
+++ SpookyJam/Assets/Scripts/Party.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Party : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private float _waitTime = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class Party : MonoBehaviour
     IEnumerator StartPartyJump()
     {
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(_waitTime);
         _animator.SetTrigger("Party");
     }
 }
