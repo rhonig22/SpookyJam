99c1e417d585b5dde0fc6a2b006142e3fdf1338d
rhonig22 <ryan.honig22@outlook.com>
2025-09-25 12:00:30 -0700

    Candle animation and World 2 Hallway
    - implement candle lighting animation
    - create world 2 hallway
    - more overworld level design

--- SpookyJam/Assets/Scripts/Objects/Doors/OverWorldDoor.cs
+++ SpookyJam/Assets/Scripts/Objects/Doors/OverWorldDoor.cs
@@ -10,6 +10,25 @@ public class OverWorldDoor : Door
     [SerializeField] private int _world;
     [SerializeField] private int _completedLevelsRequirement;
     [SerializeField] private bool _hasLock;
+    [SerializeField] private GameObject _candlePrefab;
+    [SerializeField] private Transform[] _candleLocations;
+    private List<Candle> _candles = new List<Candle>();
+    private bool _allCandlesLit = false;
+
+    private void Start()
+    {
+        if (!_hasLock)
+            return;
+
+        for (var i  = 0; i < _completedLevelsRequirement; i++)
+        {
+            var candle = GameObject.Instantiate(_candlePrefab);
+            candle.transform.position = _candleLocations[i].position;
+            candle.transform.parent = _candleLocations[i];
+            _candles.Add(candle.GetComponent<Candle>());
+        }
+    }
+
     protected override void OpenDoor()
     {
         if (!_hasLock || SaveDataManager.Instance.IsWorldUnlocked(_world))
@@ -20,7 +39,33 @@ public class OverWorldDoor : Door
         }
     }
 
+    public void HideCandles()
+    {
+        foreach (var candle in _candles)
+        {
+            candle.gameObject.SetActive(false);
+        }
+    }
+
+    public void LightCandles(int count)
+    {
+        StartCoroutine(LightCandlesWithDelay(count));
+    }
+
+    private IEnumerator LightCandlesWithDelay(int count)
+    {
+        for (var i = 0;i < count;i++)
+        {
+            _candles[i].LightCandle();
+            yield return new WaitForSeconds(.2f);
+        }
+
+        if (count == _completedLevelsRequirement)
+            _allCandlesLit = true;
+    }
+
     public int GetWorld() { return _world; }
     public bool HasLock() { return _hasLock; }
+    public bool AreAllCandlesLit() { return _allCandlesLit; }
     public int CompletedLevelsRequirement() { return _completedLevelsRequirement; }
 }
